43b2296b37bdf0863fa2a4c970e511b0caf4238d
jacobjamessimon <jacobjamessimon@gmail.com>
2023-07-22 15:46:55 +0100

    rebuilt using old directory structure

--- app/[username]/[slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page({ }) {
-  return (
-    <main>
-      <h1>Edit user post!</h1>
-    </main>
-  )
-}
--- app/[username]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page({ }) {
-  return (
-    <main>
-        <h1>Hello user profile</h1>
-    </main>
-  )
-}
